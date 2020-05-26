0cdd408ec14fdc1761d8924c704da0b3f4ed801e
JonathanCen <jcen3@binghamton.edu>
2020-05-26 17:17:44 -0400

    Simple Quiz App Complete

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var choiceOne: UIButton!
+    @IBOutlet weak var choiceTwo: UIButton!
+    @IBOutlet weak var choiceThree: UIButton!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    var quizBrain = QuizBrain()
+    
+    
+    var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        
+        if (quizBrain.checkAnswer(userAnswer)){
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUI(){
+        choiceOne.setTitle(quizBrain.getQuizChoices()[0], for: .normal)
+        choiceTwo.setTitle(quizBrain.getQuizChoices()[1], for: .normal)
+        choiceThree.setTitle(quizBrain.getQuizChoices()[2], for: .normal)
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        choiceOne.backgroundColor = UIColor.clear
+        choiceTwo.backgroundColor = UIColor.clear
+        choiceThree.backgroundColor = UIColor.clear
+    }
+    
+    
+}
+
